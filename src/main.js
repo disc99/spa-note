b82ac3400274bb72f5e94e2f53278d01b6ffd746
d.kanehira <disc99.g@gmail.com>
2017-04-23 15:04:00 +0900

    Add router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(
-    <div>Hello SPA!</div>,
-    document.getElementById('app')
+ReactDom.render((
+    <Router hisotry={browserHisory}>
+        <Route component={App}>
+            <Route path="/" component={Dashboard}>
+                <Route path="notes/:id/edit" component={NoteEdit} />
+            </Route>
+            <Route path="notes/:id" component={Note} />
+            <Route path="starred" component={Starred} />
+        </Route>
+    </Router>
+    ),document.getElementById('app')
 );
